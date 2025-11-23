8d8d815157438197742b7bf5d6015e5f3cf6c4f5
viktor <owlmajin@icloud.com>
2025-11-24 00:21:05 +1000

    2025-11-fall: Add Kotlin basics

---
+++ 2025-11-fall/01-kotlin-basics/src/_01_Variables/VarAndVals.kt
@@ -0,0 +1,35 @@
+package _01_Variables
+
+fun main() {
+    val x: Int = 5
+
+
+
+//    var x: Int = 5
+//    x += 1
+
+
+
+
+//    val x = 5
+
+
+
+
+//    val x = 5
+//    val c: Int
+//    c = 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ 2025-11-fall/01-kotlin-basics/src/_03_Functions/Sum.kt
@@ -0,0 +1,11 @@
+package _03_Functions
+
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun main() {
+    val result = sum(a = 2, b = 5)
+
+    println(result)
+}
