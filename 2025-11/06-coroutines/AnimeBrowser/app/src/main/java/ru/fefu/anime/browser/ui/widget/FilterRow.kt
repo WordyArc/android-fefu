76f2cc6c6cb59fdc8d037c41e84a99bfe5d0e126
viktor <ashenarx@icloud.com>
2026-01-20 03:57:22 +1000

    2025-11: Add updated AnimeBrowser from lesson 06-coroutines

---
+++ 2025-11/06-coroutines/AnimeBrowser/app/src/main/java/ru/fefu/anime/browser/ui/widget/FilterRow.kt
@@ -0,0 +1,27 @@
+package ru.fefu.anime.browser.ui.widget
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import ru.fefu.anime.browser.model.AnimeFilter
+
+@Composable
+fun FilterRow(
+    filter: AnimeFilter,
+    onFilterChange: (AnimeFilter) -> Unit
+) {
+    Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+        TextButton(
+            onClick = { onFilterChange(AnimeFilter.ALL) },
+            enabled = filter != AnimeFilter.ALL
+        ) { Text(if (filter == AnimeFilter.ALL) "• All" else "All") }
+
+        TextButton(
+            onClick = { onFilterChange(AnimeFilter.FAVOURITES) },
+            enabled = filter != AnimeFilter.FAVOURITES
+        ) { Text(if (filter == AnimeFilter.FAVOURITES) "• Favourites" else "Favourites") }
+    }
+}
